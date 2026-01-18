b43f79430e85019508baa2a252752dcc50f30f8d
Ellen Red <99109157+ellenrapps@users.noreply.github.com>
2026-01-18 09:36:22 +0800

    Create AlarmKaniWindow.xaml.cs

---
+++ AlarmKaniWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace alarm_vs_preview61825at113pm
+{
+    
+    public partial class AlarmKaniWindow : Window
+    {
+        public string myText { get; set; }
+        public AlarmKaniWindow(string myText)
+        {
+            InitializeComponent();
+            this.Topmost = true;
+            Mb1_0Tb.Text = myText;
+        }
+
+
+        private void AlarmNoticeXButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void AlarmNoticeOkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
