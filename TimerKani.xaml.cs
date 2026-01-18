3bbdb0b4ea230c0e1f1786a9a1b3bcf36b86249b
Ellen Red <99109157+ellenrapps@users.noreply.github.com>
2026-01-18 09:52:47 +0800

    Create TimerKani.xaml.cs

---
+++ TimerKani.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+
+namespace alarm_vs_preview61825at113pm
+{
+    
+    public partial class TimerKani : Window
+    {
+        public TimerKani()
+        {
+            InitializeComponent();
+            this.Topmost = true;
+        }
+
+        private void TimerNoticeXButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TimerNoticeOkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
