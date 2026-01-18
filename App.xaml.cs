0ee6ca5f70065082df843c1babeeb749ca6991d5
Ellen Red <99109157+ellenrapps@users.noreply.github.com>
2026-01-18 09:38:00 +0800

    Create App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace alarm_vs_preview61825at113pm
+{
+    
+    public partial class App : Application
+    {
+    }
+
+}
