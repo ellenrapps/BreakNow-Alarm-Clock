0b48c30f0ef34b9749a7e8506735ed9e6246fc22
Ellen Red <99109157+ellenrapps@users.noreply.github.com>
2026-01-18 09:57:33 +0800

    Create WindowFaq.xaml.cs

---
+++ WindowFaq.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+
+namespace alarm_vs_preview61825at113pm
+{
+    
+    public partial class WindowFaq : Window
+    {
+        public WindowFaq()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.Manual;
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+        }
+
+        private void WindowFaqCloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void WindowFaqMinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+    }
+}
